f733cffe28839533e18279311950c6df6195be4d
arthurbs00 <61774770+arthurbs00@users.noreply.github.com>
2022-05-20 08:35:52 +1100

    Update apiPowerBi.py

--- apiPowerBi.py
+++ apiPowerBi.py
@@ -1,2 +1,7 @@
-"Get https://api.pwerbi.cm/v1.0/myorg/groups"
+import requests
+def testF() :
+  posts = requests.get("https://api.powerbi.com/v1.0/myorg/datasets/cfafbeb1-8037-4d0c-896e-a46fb27ff229")
+  text_post = posts.text
+  return posts
 
+print(testF())
