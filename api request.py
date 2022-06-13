7df0e59da85a8853ef70b8ab80e652f79aa352f0
arthurbs00 <61774770+arthurbs00@users.noreply.github.com>
2022-06-13 16:17:08 +1100

    Update api request.py

--- api request.py
+++ api request.py
@@ -6,7 +6,7 @@ import requests
 
 
 urlconnect="https://prod-apsoutheast-a.online.tableau.com/api/3.15/auth/signin"
-
+urlcreagroup="https://prod-apsoutheast-a.online.tableau.com/api/3.1/sites/site-id/groups"
 def sessionsF() :
     body='<tsRequest> <credentials name="votredumuffe-8833@yopmail.com" password="Votredumuffe.8833"><site contentUrl="" /></credentials></tsRequest>'
 
@@ -16,6 +16,17 @@ def sessionsF() :
     #temp = json.loads(text_post)
     return text_post
 
-sessions = sessionsF()
+def creation_groupF() :
+    body='<tsRequest><group name="TC18"/></tsRequest>'
+    data="token"
+    posts = requests.post(urlconnect,body,data)
+  
+    text_post = posts.text
+    #temp = json.loads(text_post)
+    return text_post
 
-print(sessions)
+
+sessions = sessionsF()
+creation_group = creation_groupF()
+
+print(creation_group)
