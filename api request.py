65393892a4b6663925dca29e7a4162058f24bba4
arthurbs00 <61774770+arthurbs00@users.noreply.github.com>
2022-06-14 16:16:52 +1100

    Update api request.py

--- api request.py
+++ api request.py
@@ -1,33 +1,52 @@
-
-
-
-
+import json
+import site
 import requests
 
 
-urlconnect="https://prod-apsoutheast-a.online.tableau.com/api/3.15/auth/signin"
-urlcreagroup="https://prod-apsoutheast-a.online.tableau.com/api/3.1/sites/site-id/groups"
+urlconnect="https://prod-apsoutheast-a.online.tableau.com/api/3.5/auth/signin"
+
 def sessionsF() :
     body='<tsRequest> <credentials name="votredumuffe-8833@yopmail.com" password="Votredumuffe.8833"><site contentUrl="tartempion" /></credentials></tsRequest>'
 
     posts = requests.post(urlconnect,body)
-    
-    text_post = posts.content
+    i=0
+    text_post = posts.text
+    temp=""
+    token=""
+    siteid=""
     #token=text_post.token
     #temp = json.loads(text_post)
-    return text_post
+    for i in range(len(text_post)) : 
+        if text_post[i] == " " : 
+            temp=text_post[i+1]+text_post[i+2]+text_post[i+3]+text_post[i+4]+text_post[i+5]
+            if temp == "token" : 
+                j=i
+                while text_post[j+8] !='"' :
+                    token+=text_post[j+8]
+                    j+=1
+            temp=text_post[i+1]+text_post[i+2]
+            if temp == "id" : 
+                y=i
+                while text_post[y+5] !='"' :
+                    siteid+=text_post[y+5]
+                    y+=1
+                break
+    return token,siteid
 
-def creation_groupF() :
-    body='<tsRequest><group name="TC18"/></tsRequest>'
-    data="token"
-    posts = requests.post(urlconnect,body,data)
+token,siteid = sessionsF()
+urlcreagroup="https://prod-apsoutheast-a.online.tableau.com/api/3.5/sites/"+siteid+"/projects"
+
+def creation_projectF(token) :
+    header=token
+    body='<tsRequest><project name="datadev prject" description="thisis a new project"/></tsRequest>'
+    posts = requests.post(urlconnect,header,body)
   
     text_post = posts.text
     #temp = json.loads(text_post)
     return text_post
 
 
-sessions = sessionsF()
-creation_group = creation_groupF()
 
-print(sessions)
+creation_group = creation_projectF(token)
+
+print(creation_group)
